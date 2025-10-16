6f82a2f7f117598c442f26ef1986211e409a8751
Guilherme Augusto  Santos <guipratiko@MacBook-Pro-de-Guilherme.local>
2025-10-16 16:44:13 -0300

    correcao

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()]
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL?.replace('/api', '') || 'http://localhost:4800',
+          changeOrigin: true
+        }
+      }
+    }
+  }
 })
 
